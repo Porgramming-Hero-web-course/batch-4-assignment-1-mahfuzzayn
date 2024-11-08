142b4c2194e5a67ee400378b252bdc1d130dd4d7
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-11-08 22:00:01 +0600

    Problem 3 submit

---
+++ problems/problem-3.ts
@@ -0,0 +1,22 @@
+{
+// Problem 3
+    
+const countWordOccurrences = (sentence: string, word: string): number  =>{
+    const normalizedSentence: string = sentence.toLowerCase();
+    const words: string[] = normalizedSentence.split(' ')
+    let occurrences: number = 0;
+
+    words.forEach((str: string)=> {
+        if (str === word) {
+            occurrences++
+        }
+    })
+
+    return occurrences;
+}
+    
+const result = countWordOccurrences("I love typescript", "typescript");
+
+console.log(result);
+
+}
