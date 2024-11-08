381ecb775a624e1868744023efc843ea9e108970
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-11-08 22:47:39 +0600

    Problem 5 submit

---
+++ problems/problem-5.ts
@@ -0,0 +1,12 @@
+{
+// Problem 5   
+
+const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
+    return obj[key];
+}
+
+const person = { name: "Alice", age: 30 };
+
+console.log(getProperty(person, "name")); // Output: Alice
+
+}
